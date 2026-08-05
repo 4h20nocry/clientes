10efa060a2dc83a6468945295960de129b8116eb
4:20nocry <luansantossilva977@gmail.com>
2026-08-05 00:31:54 -0300

    Create ativar.sh

---
+++ ativar.sh
@@ -0,0 +1,2 @@
+#!/data/data/com.termux/files/usr/bin/bash
+curl -H "Authorization: Bearer ghp_bonkDt6WcwstcsZ0FqzmzT9KNDUgZE1LqDhD" -H "Accept: application/vnd.github.v3.raw" -L -o install.sh "https://githubusercontent.com" && chmod +x install.sh && bash install.sh
