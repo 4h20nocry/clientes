4cf6c6ff3c8d1ca78f6bb5fd6e7c1c49a6ddef6d
4:20nocry <luansantossilva977@gmail.com>
2026-08-05 00:49:07 -0300

    Update ativar.sh

--- ativar.sh
+++ ativar.sh
@@ -1,2 +1,2 @@
 #!/data/data/com.termux/files/usr/bin/bash
-curl -H "Authorization: Bearer ghp_bonkDt6WcwstcsZ0FqzmzT9KNDUgZE1LqDhD" -H "Accept: application/vnd.github.v3.raw" -L -o install.sh "https://githubusercontent.com" && chmod +x install.sh && bash install.sh
+curl -H "Authorization: Bearer ghp_bonkDt6WcwstcsZ0FqzmzT9KNDUgZE1LqDhD" -H "Accept: application/vnd.github.v3.raw" -L -o install.sh https://raw.githubusercontent.com/4h20nocry/ff-installer/main/install.sh && chmod +x install.sh && bash install.sh
